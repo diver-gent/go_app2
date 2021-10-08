71b824dce60d1ea0481320982c2d7205f89100d3
Bill Townsley <bill.townsley@armory.io>
2021-10-08 14:47:40 -0700

    baseline

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	greeting := os.Getenv("GREETING")
+	greeting := os.Getenv("GREET")
 	fmt.Fprintf(w, "%s, 世界\n", greeting)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8888", nil))
+	log.Fatal(http.ListenAndServe(":7777", nil))
 }
